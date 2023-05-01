4ff28d19269c036fabe5f4803767f3c3ecc91991
charlicastelli <castellicharli@gmail.com>
2023-05-01 13:57:01 -0300

    spinner

--- front-end/src/app/components/home/home.component.ts
+++ front-end/src/app/components/home/home.component.ts
@@ -1,21 +1,19 @@
 import { Component } from '@angular/core';
 import { ContactsService } from '../service/contacts/contacts.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Model } from 'src/app/shared/model/model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   contacts = [];
   displayedColumns: string[] = ['name', 'email', 'phone'];
 
-
-  constructor(
-    private contactService: ContactsService,
-    private router: Router
-    ) { 
+  constructor(private contactService: ContactsService, private router: Router) {
     this.getContacts();
   }
 
@@ -23,12 +21,13 @@ export class HomeComponent {
     this.contactService.getAllContacts().subscribe(
       (data) => {
         this.contacts = data;
-      }, 
+        console.log(this.contacts);
+      },
       (error) => {
         console.log('Aconteceu um erro');
       }
     );
-  };
+  }
 
   clickedRows(row: any) {
     this.router.navigate(['contact-detail', row.id]);
